94f8e92a2195d7b638d1625b8517279f2d65fb64
Sinaloit <Sinaloit@gmail.com>
2014-08-12 19:26:14 -0700

    Need to require Jarod to get references to Apollo

--- spec/Get_WeaselString_spec.lua
+++ spec/Get_WeaselString_spec.lua
@@ -1,3 +1,5 @@
+require "Jarod"
+
 describe('Get_WeaselString Functionality Testing', function()
   it ('tests TargetFrame_ShortNumberWhole', function()
     assert.is_equal(Get_WeaselString(Apollo.GetString("TargetFrame_ShortNumberWhole"),5),"5k")
