f1c557fd2d3f7e16929b7ffa3c9841471554daaa
Sinaloit <Sinaloit@gmail.com>
2014-08-15 07:14:34 -0700

    More spec adjustments

--- spec/ApolloPackages_spec.lua
+++ spec/ApolloPackages_spec.lua
@@ -1,11 +1,12 @@
 require "Jarod"
 
-describe('ApolloPackage Functionality Testing', function()
-  local PackageOne = { OnLoad = print("PackageOne Load") }
-  local PackageTwo = { OnLoad = print("PackageTwo Load") }
-  local MAJOR = "TestPackage-1.0"
-  local MINOR_ONE, MINOR_TWO = 9, 4
+local PackageOne = { OnLoad = print("PackageOne Load") }
+local PackageTwo = { OnLoad = print("PackageTwo Load") }
 
+local MAJOR = "TestPackage-1.0"
+local MINOR_ONE, MINOR_TWO = 9, 4
+
+describe('ApolloPackage Functionality Testing', function()
   it ('accepts a Package', function()
     Apollo.RegisterPackage(PackageOne, MAJOR, MINOR_ONE, {})
   end)
@@ -13,13 +14,13 @@ describe('ApolloPackage Functionality Testing', function()
   it ('returns the Package', function()
     local retAPkg = Apollo.GetPackage(MAJOR)
     local retPkg = retAPkg and retAPkg.tPackage or nil
-    assert.are.equal(retPkg, PackageOne)
+    assert.are.equal(PackageOne, retPkg)
   end)
 
   it ('only accepts the highst minor version', function()
     Apollo.RegisterPackage(PackageTwo, MAJOR, MINOR_TWO, {})
     local retAPkg = Apollo.GetPackage(MAJOR)
     local retPkg = retAPkg and retAPkg.tPackage or nil
-    assert.are.equal(retPkg, PackageOne)
+    assert.are.equal(PackageOne, retPkg)
   end)
 end)
