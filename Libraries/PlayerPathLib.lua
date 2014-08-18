e4a8d30c32f65250d4ff5cf0537416e018f7e537
Marius Ungureanu <teromario@yahoo.com>
2014-08-18 03:41:55 +0300

    Implement PlayerPathLib enums

--- Libraries/PlayerPathLib.lua
+++ Libraries/PlayerPathLib.lua
@@ -72,5 +72,19 @@ function PlayerPathLib.SetScannerName()
 end
 function PlayerPathLib.GetScannerName()
 end
+PlayerPathLib.PathRewardType_Item = 0
+PlayerPathLib.PathRewardType_Spell = 1
+PlayerPathLib.PathRewardType_Title = 2
+PlayerPathLib.PathRewardType_Quest = 3
+PlayerPathLib.PathRewardType_ScanBot = 4
+
+PlayerPathLib.PlayerPathType_Soldier = 0
+PlayerPathLib.PlayerPathType_Settler = 1
+PlayerPathLib.PlayerPathType_Scientist = 2
+PlayerPathLib.PlayerPathType_Explorer = 3
+
+PlayerPathLib.SettlerInfrastructureState_Inactive = 0
+PlayerPathLib.SettlerInfrastructureState_Building = 1
+PlayerPathLib.SettlerInfrastructureState_Built = 2
 
 return PlayerPathLib
