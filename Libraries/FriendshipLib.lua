701ea5c0217e4e8e6c9cfd434ae863dc0a8fe97e
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-08-19 17:07:33 +0300

    Fix FriendshipLib enums.

--- Libraries/FriendshipLib.lua
+++ Libraries/FriendshipLib.lua
@@ -82,4 +82,51 @@ end
 function FriendshipLib.NeighborInvite()
 end
 
+FriendshipLib.CharacterFriendshipType_Account = 8
+FriendshipLib.CharacterFriendshipType_Friend = 0
+FriendshipLib.CharacterFriendshipType_Ignore = 1
+FriendshipLib.CharacterFriendshipType_Rival = 3
+FriendshipLib.FriendshipResponse_Accept = 1
+FriendshipLib.FriendshipResponse_Decline = 2
+FriendshipLib.FriendshipResponse_Ignore = 3
+FriendshipLib.FriendshipResponse_Mutual = 0
+FriendshipLib.FriendshipResult_BlockedForStrangers = 36
+FriendshipLib.FriendshipResult_Busy = 9
+FriendshipLib.FriendshipResult_CannotInviteSelf = 29
+FriendshipLib.FriendshipResult_ContainsProfanity = 31
+FriendshipLib.FriendshipResult_FriendsBlocked = 26
+FriendshipLib.FriendshipResult_FriendshipNotFound = 5
+FriendshipLib.FriendshipResult_InvalidAutoResponse = 37
+FriendshipLib.FriendshipResult_InvalidEmail = 34
+FriendshipLib.FriendshipResult_InvalidInviteNote = 35
+FriendshipLib.FriendshipResult_InvalidNote = 10
+FriendshipLib.FriendshipResult_InvalidPublicDisplayName = 33
+FriendshipLib.FriendshipResult_InvalidPublicNote = 32
+FriendshipLib.FriendshipResult_InvalidType = 6
+FriendshipLib.FriendshipResult_LocationsBusy = 28
+FriendshipLib.FriendshipResult_MaxFriends = 11
+FriendshipLib.FriendshipResult_MaxIgnored = 13
+FriendshipLib.FriendshipResult_MaxRivals = 12
+FriendshipLib.FriendshipResult_NameUnavailable = 39
+FriendshipLib.FriendshipResult_PlayerAlreadyFriend = 3
+FriendshipLib.FriendshipResult_PlayerAlreadyIgnored = 20
+FriendshipLib.FriendshipResult_PlayerAlreadyNeighbor = 19
+FriendshipLib.FriendshipResult_PlayerAlreadyRival = 18
+FriendshipLib.FriendshipResult_PlayerConsideringOtherFriend = 16
+FriendshipLib.FriendshipResult_PlayerNotFound = 0
+FriendshipLib.FriendshipResult_PlayerNotFriend = 15
+FriendshipLib.FriendshipResult_PlayerNotIgnored = 23
+FriendshipLib.FriendshipResult_PlayerNotNeighbor = 24
+FriendshipLib.FriendshipResult_PlayerNotOfThisRealm = 25
+FriendshipLib.FriendshipResult_PlayerNotRival = 22
+FriendshipLib.FriendshipResult_PlayerOffline = 4
+FriendshipLib.FriendshipResult_PlayerOnIgnored = 21
+FriendshipLib.FriendshipResult_RealmNotFound = 1
+FriendshipLib.FriendshipResult_RequestDenied = 2
+FriendshipLib.FriendshipResult_RequestNotFound = 7
+FriendshipLib.FriendshipResult_RequestSent = 17
+FriendshipLib.FriendshipResult_RequestTimedOut = 8
+FriendshipLib.FriendshipResult_ThrottledRequest = 30
+FriendshipLib.FriendshipResult_UnableToProcess = 14
+
 return FriendshipLib
