1075b920895924082069bb83bfe42021b9a760a2
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-08-20 03:31:25 +0300

    Fix PetCustomizationLib enums.

--- Libraries/PetCustomizationLib.lua
+++ Libraries/PetCustomizationLib.lua
@@ -7,29 +7,38 @@ end
 function PetCustomizationLib.CanCustomize()
 end
 PetCustomizationLib.MountSlot = {
-  Front = 1,
-  Back = 2,
-  Left = 3,
-  Right = 4
+  Front = 0,
+  Back = 1,
+  Left = 2,
+  Right = 3
 }
 PetCustomizationLib.HoverboardSlot = {
-  Front = 1,
-  Back = 2,
-  Sides = 3
+  Front = 0,
+  Back = 1,
+  Sides = 2
 }
 PetCustomizationLib.PetType = {
-  Scanbot = 1,
-  GroundMount = 2,
-  HoverBoard = 3
+  Scanbot = 0,
+  GroundMount = 1,
+  HoverBoard = 2
 }
 PetCustomizationLib.PetCustomizeResult = {
-  PetTypeNotSupported = 1,
-  InvalidName = 2,
-  InvalidSlot = 3,
-  UnknownFlairType = 4,
+  NotUnlocked = 1,
+  PetTypeNotSupported = 2,
+  UnknownFlairType = 3,
+  InvalidObject = 4,
   InvalidFlair = 5,
-  NotUnlocked = 6,
-  InvalidObject = 7
+  InvalidSlot = 6,
+  InvalidName = 7
 }
+PetCustomizationLib.HoverboardAttachmentPoint = 65
+PetCustomizationLib.PetFlairType_GroundMountBack = 3
+PetCustomizationLib.PetFlairType_GroundMountFront = 2
+PetCustomizationLib.PetFlairType_GroundMountSide = 4
+PetCustomizationLib.PetFlairType_HoverMountBack = 6
+PetCustomizationLib.PetFlairType_HoverMountFront = 5
+PetCustomizationLib.PetFlairType_HoverMountSide = 7
+PetCustomizationLib.PetFlairType_ScanBotRom = 0
+PetCustomizationLib.PetFlairType_ScanBotVanity = 1
 
 return PetCustomizationLib
